92c7a126ebc4de01c156d824595fde51776589c7
Sandro <sandro.bugmann@posthaus.com.br>
2022-11-16 14:35:50 -0300

    Teste condicional

---
+++ src/sintaxe/variaveis/fluxo/TestaCondicional.java
@@ -0,0 +1,21 @@
+package sintaxe.variaveis.fluxo;
+
+public class TestaCondicional {
+	
+	public static void main(String[] args) {
+		System.out.println("testando condicionais");
+		int idade = 16;
+		int quantidadePessoas = 3;
+		if (idade >= 18) {
+			System.out.println("Você tem mais de 18 anos");
+		}else {
+				
+				if (quantidadePessoas >= 2) {
+					System.out.println("Você é menor de 18 anos, porém está acompanhado, então pode entrar");
+	
+				} else {
+					System.out.println("Infelizmente você não pode entrar");
+				}
+		}
+	}
+}
---
+++ src/sintaxe/variaveis/fluxo/TesteIR.java
@@ -0,0 +1,17 @@
+package sintaxe.variaveis.fluxo;
+
+public class TesteIR {
+
+	public static void main(String[] args) {
+		
+		double salario = 4000.0;
+	
+		if (salario >= 1900 && salario <= 2800) {
+			System.out.println("IR é de 7.5% e pode deduzir na declaração o valor de R$ 142");
+		} else if (salario >= 2800.01 && salario <= 3751.0) {
+			System.out.println(" IR é de 15% e pode deduzir R$ 350");
+		} else if (salario >= 3751.0 && salario <= 4664) {
+			System.out.println("IR é de 22.5% e pode deduzir R$ 636");
+		}
+	}
+}
