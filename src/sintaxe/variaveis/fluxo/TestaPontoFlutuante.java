1af242204f6ed863568f87703e1e3606d9f52362
Sandro <sandro.bugmann@posthaus.com.br>
2022-11-16 10:25:33 -0300

    ponto flutuante

---
+++ src/sintaxe/variaveis/fluxo/TestaPontoFlutuante.java
@@ -0,0 +1,21 @@
+package sintaxe.variaveis.fluxo;
+
+public class TestaPontoFlutuante {
+
+	public static void main(String[] args) {
+		
+		double salario;
+		salario = 1250.70;
+		System.out.println("Meu salário é "+salario);
+		
+		int divisao = 5/2;
+		System.out.println(divisao);
+		
+		double outraDivisao = 5.0/2;
+		System.out.println(outraDivisao);
+		
+		double valor1 = 0.2;
+		double valor2 = 0.1;
+		System.out.println(valor1 + valor2);
+	}
+}
---
+++ src/sintaxe/variaveis/fluxo/TesteVariaveis.java
@@ -0,0 +1,15 @@
+package sintaxe.variaveis.fluxo;
+
+public class TesteVariaveis {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Olá novo teste");
+		
+		int idade;
+		idade = 41;
+		
+		System.out.println(idade);
+	}
+
+}
